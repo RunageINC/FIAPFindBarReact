e5d4de9f85c1aade409b72895843fc3057fdf842
ArthurGomesAraujo <arthur.aaraujo@mercadolivre.com>
2022-03-21 22:16:01 -0300

    removing unused css

--- components/molecules/MusicalStyles/MusicalStyles.jsx
+++ components/molecules/MusicalStyles/MusicalStyles.jsx
@@ -3,8 +3,6 @@ import Card from 'react-bootstrap/Card';
 import BarType from "../../atoms/BarType/BarType";
 import BarLiveMusic from "../../atoms/BarLiveMusic/BarLiveMusic";
 
-import classes from './MusicalStyles.module.css';
-
 const MusicalStyles = ({type, hasLiveMusic, musicalStyles }) => {
   <Card body className="mt-3">
     <BarType type={type} musicalStyles={musicalStyles}/>
